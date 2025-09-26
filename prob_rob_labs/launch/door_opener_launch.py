6c31c1ace7749e6e67a4a9bd3737b25a1ec5a612
James Zhang <tz2642@columbia.edu>
2025-09-25 20:30:23 -0400

    Add prob3,4 to lab2

--- prob_rob_labs/launch/door_opener_launch.py
+++ prob_rob_labs/launch/door_opener_launch.py
@@ -10,10 +10,12 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value='true',
                               description='set to true for simulation'),
+        DeclareLaunchArgument('forward', default_value = '0.70', description='Forward speed of robot'),
         Node(
             package='prob_rob_labs',
             executable='door_opener',
             name='door_opener',
-            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')},
+                {'forward_speed': LaunchConfiguration('forward_speed')}]
         )
-    ])
+        ])
--- prob_rob_labs/src/door_opener/door_opener.py
+++ prob_rob_labs/src/door_opener/door_opener.py
@@ -1,5 +1,7 @@
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
 
 
 heartbeat_period = 0.1
@@ -10,9 +12,45 @@ class DoorOpener(Node):
         super().__init__('door_opener')
         self.log = self.get_logger()
         self.timer = self.create_timer(heartbeat_period, self.heartbeat)
+        self.door_pub = self.create_publisher(Float64, '/hinged_glass_door/torque', 5)
+        self.cmd_pub = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.declare_parameter ('forward_speed', 0.7)
+        self.speed = self.get_parameter('forward_speed').get_parameter_value().double_value
+
+        #State machine
+        self.state = "Open_door"
+        self.start_time = self.get_clock().now()
 
     def heartbeat(self):
         self.log.info('heartbeat')
+        elapsed = (self.get_clock().now() - self.start_time).nanoseconds * 1e-9
+        
+        if self.state == "Open_door":
+            self.door_pub.publish(Float64(data=5.0))
+            if elapsed > 3.0:
+                self._next("Move_Forward")
+
+        elif self.state == "Move_Forward":
+            twist = Twist(); twist.linear.x = self.speed
+            self.cmd_pub.publish(twist)
+            if elapsed >7.0:
+                self._next("STOP")
+
+        elif self.state == "STOP":
+            self.cmd_pub.publish(Twist())
+            self._next("Closed_door")
+
+        elif self.state == "Closed_door":
+            self.door_pub.publish(Float64(data=-5.0))
+            if elapsed > 3.0:
+                self.get_logger().info("Mission Done")
+                self.timer.cancel()
+
+    def _next(self, new_state):
+        self.state = new_state
+        self.start_time = self.get_clock().now()
+        self.get_logger().info(f"Switch to {new_state}")
+
 
     def spin(self):
         rclpy.spin(self)
